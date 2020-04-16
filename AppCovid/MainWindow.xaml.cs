fcd187d0e067119d076e8263fc7c03198d5fb4cb
martina biagioni <martinabiagioni12@gmail.com>
2020-04-16 16:04:16 +0100

    Ho inserito il codice per far scorrere il file, ho utilizzao il foreach e all'interno ho creato un'istanza della classe e ho inserito le proprietà all'interno di una lista

--- AppCovid/MainWindow.xaml.cs
+++ AppCovid/MainWindow.xaml.cs
@@ -21,6 +21,7 @@ namespace AppCovid
     /// </summary>
     public partial class MainWindow : Window
     {
+        List<DatiCovid> dati;
         public MainWindow()
         {
             InitializeComponent();
@@ -28,10 +29,50 @@ namespace AppCovid
 
         private void Btn_VisualizzaDati_Click(object sender, RoutedEventArgs e)
         {
+            dati = new List<DatiCovid>();
             string path = @"DatiCovid.xml";
             XDocument xmlDoc = XDocument.Load(path);
             XElement xmlroot = xmlDoc.Element("root");
             var xmlrow = xmlroot.Elements("row");
+            foreach (var item in xmlrow)
+            {
+                XElement xmlData = item.Element("data");
+                XElement xmlStato = item.Element("stato");
+                XElement xmlSintomatici = item.Element("ricoverati_con_sintomi");
+                XElement xmlTerapiaIntensiva = item.Element("terapia_intensiva");
+                XElement xmlTotaleOspedalizzati = item.Element("totale_ospedalizzati");
+                XElement xmlIsolamentoDomiciliare = item.Element("isolamento_domiciliare");
+                XElement xmlTotalePositivi = item.Element("totale_positivi");
+                XElement xmlVariazionePositivi = item.Element("variazione_totale_positivi");
+                XElement xmlNuoviPositivi = item.Element("nuovi_positivi");
+                XElement xmlDimessiGuariti = item.Element("dimessi_guariti");
+                XElement xmlDeceduti = item.Element("deceduti");
+                XElement xmlTotaleCasi = item.Element("totale_casi");
+                XElement xmlTamponi = item.Element("tamponi");
+                XElement xmlNoteIt = item.Element("note_it");
+                XElement xmlNoteEn = item.Element("note_en");
+
+                DatiCovid d = new DatiCovid();
+                {
+                    d.Data = Convert.ToDateTime(xmlData.Value);
+                    d.Stato = xmlStato.Value;
+                    d.Sintomatici = Convert.ToInt32(xmlSintomatici.Value);
+                    d.TerapiaIntensiva = Convert.ToInt32(xmlTerapiaIntensiva.Value);
+                    d.TotaleOspedalizzati = Convert.ToInt32(xmlTotaleOspedalizzati.Value);
+                    d.IsolamentoDomiciliare = Convert.ToInt32(xmlIsolamentoDomiciliare.Value);
+                    d.TotalePositivi = Convert.ToInt32(xmlTotalePositivi.Value);
+                    d.VariazionePositivi = Convert.ToInt32(xmlVariazionePositivi.Value);
+                    d.NuoviPositivi = Convert.ToInt32(xmlNuoviPositivi.Value);
+                    d.DimessiGuariti = Convert.ToInt32(xmlDimessiGuariti.Value);
+                    d.Deceduti = Convert.ToInt32(xmlDeceduti.Value);
+                    d.TotaleCasi = Convert.ToInt32(xmlTotaleCasi.Value);
+                    d.Tamponi = Convert.ToInt32(xmlTamponi.Value);
+                    d.NoteIt = xmlNoteIt.Value;
+                    d.NoteEn = xmlNoteEn.Value;
+                    dati.Add(d);
+                }
+
+            }
         }
     }
 }
